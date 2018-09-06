8129c4f8bcbd26777ffd7e093b65c6f77e71e0e8
silex189 <antoniojm89@gmail.com>
2018-09-06 22:36:36 +0100

    thunder exercise

---
+++ Book_utilities/src/chapter2/Sounds.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class Sounds {
+
+	public static void main(String[] args) {
+		final double thunder = 1100;
+		double time = 7.2;
+		
+		double distance = time * thunder;
+		System.out.println("Thunder is " + distance + " foots away"); 
+		
+	}
+
+}
