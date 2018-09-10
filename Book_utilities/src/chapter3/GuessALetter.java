ea139c1bf18da6f7e362e0a8d6f4089a5f1b281d
silex189 <antoniojm89@gmail.com>
2018-09-11 00:02:26 +0100

    Chapter 3 finished

--- Book_utilities/src/chapter3/GuessALetter.java
+++ Book_utilities/src/chapter3/GuessALetter.java
@@ -5,20 +5,27 @@ public class GuessALetter {
 
 	public static void main(String[] args) 
 			throws java.io.IOException {
+
+			char character, ignore, answer = 'k';
 		 
 
-			char character, answer = 'k';
-			System.out.println("I'm thinking of a letter between a and z.");
-			System.out.println("Can you guess it: ");
+			do {
+					System.out.println("I'm thinking of a letter between a and z.");
+					System.out.println("Can you guess it: ");
 
-			character = (char) System.in.read();
+					character = (char) System.in.read();
+					do {
+						ignore = (char) System.in.read();
+					} while (ignore != '\n');
 
-			if (character == answer) System.out.println("**Right**");
-			else {
-				System.out.println("... Sorry, you are ");
-				if(character < answer) System.out.println("too low");
-				else System.out.println("too high");
-			}
+					if (character == answer) System.out.println("**Right**");
+					else {
+						System.out.println("... Sorry, you are ");
+						if(character < answer) System.out.println("too low");
+						else System.out.println("too high");
+					}
+			} while (answer != character);
+			
 		
 
 	}
