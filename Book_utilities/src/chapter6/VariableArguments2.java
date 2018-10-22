a108db9cdb2ce4177f8ba938ceb31949e0f6de1c
silex189 <antoniojm89@gmail.com>
2018-10-22 13:47:33 +0100

    chapter 6 finished

---
+++ Book_utilities/src/chapter6/VariableArguments2.java
@@ -0,0 +1,21 @@
+package chapter6;
+
+public class VariableArguments2 {
+
+	static void variableArgumentsTest(String msg, int ... v) {
+		System.out.println(msg + v.length);
+		System.out.println("Contents: ");
+
+		for(int i=0; i < v.length; i++)
+			System.out.println(" arg " + i + ": " + v[i]);
+
+			System.out.println();
+	}
+	public static void main(String[] args) {
+		variableArgumentsTest("One variable arguments: ", 10);
+		variableArgumentsTest("Three variable arguments: ", 1, 2, 3);
+		variableArgumentsTest("No variable arguments");
+
+	}
+
+}
