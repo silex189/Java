794dd29119861a18e59ac188cb4f30dc709dcd9e
silex189 <antoniojm89@gmail.com>
2018-10-30 14:41:57 +0000

    synchronized done

--- Book_utilities/src/chapter11_Multiple_Threading/MyThreadExternalSynchronized.java
+++ Book_utilities/src/chapter11_Multiple_Threading/MyThreadExternalSynchronized.java
@@ -1,12 +1,12 @@
 package chapter11_Multiple_Threading;
 
-public class MyThreadSynchronized implements Runnable {
+public class MyThreadExternalSynchronized implements Runnable {
   Thread thread;
   static ArraySummator arraySummator = new ArraySummator();
   int array[];
   int answer;
 
-  MyThreadSynchronized(String name, int array[]) {
+  MyThreadExternalSynchronized(String name, int array[]) {
     thread = new Thread(this, name);
     this.array = array;
     thread.start();
@@ -15,10 +15,7 @@ public class MyThreadSynchronized implements Runnable {
   public void run() {
     System.out.println(thread.getName() + " starting.");
 
-    synchronized(arraySummator) {
-      answer = arraySummator.summatorMethod(array);
-    }
-
+    answer = arraySummator.summatorMethod(array);
     System.out.println("Sum for " + thread.getName() + " is " + answer);
 
     System.out.println(thread.getName() + " terminating.");
