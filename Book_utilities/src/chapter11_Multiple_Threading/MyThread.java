0694969c28d02a354e3129496a98927398137600
silex189 <antoniojm89@gmail.com>
2018-10-30 00:31:17 +0000

    first classes

--- Book_utilities/src/chapter11_Multiple_Threading/MyThread.java
+++ Book_utilities/src/chapter11_Multiple_Threading/MyThread.java
@@ -1,5 +1,24 @@
 package chapter11_Multiple_Threading;
 
-public class MyThread {
+class MyThread implements Runnable {
+
+	String threadName;
+
+	MyThread(String threadName) {
+		this.threadName = threadName;
+	}
+
+	public void run() {
+		System.out.println(threadName + " starting.");
+		try {
+			for (int count = 0; count < 10; count++) {
+				Thread.sleep(400);
+				System.out.println("In " + threadName + ", count is " + count);
+			}
+		} catch (InterruptedException exception) {
+			System.out.println(threadName + " interrupted.");
+		}
+		System.out.println(threadName + " terminating.");
+	}
 
 }
--- Book_utilities/src/chapter11_Multiple_Threading/UseThreads.java
+++ Book_utilities/src/chapter11_Multiple_Threading/UseThreads.java
@@ -3,8 +3,23 @@ package chapter11_Multiple_Threading;
 public class UseThreads {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Main thread starting.");
 
+		MyThread myThread = new MyThread("Child #1");
+
+		Thread newThread = new Thread(myThread);
+
+		newThread.start();
+
+		for (int i = 0; i < 50; i++) {
+			System.out.print(".");
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException exception) {
+				System.out.println("Main thread interrupted.");
+			}
+		}
+		System.out.println("Main thread ending.");
 	}
 
 }
