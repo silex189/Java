2d7602fc20271b1f48cc1225368898ec6cadf2cd
silex189 <antoniojm89@gmail.com>
2018-10-30 11:09:18 +0000

    Improved and Extended threads

--- Book_utilities/src/chapter1_Concepts/GalToLitTable.java
+++ Book_utilities/src/chapter1_Concepts/GalToLitTable.java
@@ -4,17 +4,17 @@ public class GalToLitTable {
 
 	public static void main(String[] args) {
 		double gallons, liters;
-		
+
 		int counter = 0;
-		
-		for(gallons = 1; gallons <= 30; gallons++) {
+
+		for (gallons = 1; gallons <= 30; gallons++) {
 			liters = gallons * 3.7854;
 			System.out.println(gallons + " gallons equal to " + liters + "liters");
 			counter++;
-			
-			if(counter >= 10) {
+
+			if (counter >= 10) {
 				System.out.println("");
-			counter = 0;
+				counter = 0;
 			}
 		} // prueba eclipse
 
