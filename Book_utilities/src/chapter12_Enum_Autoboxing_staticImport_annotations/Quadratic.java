9da6db312267485e99e1540cf4ad666b0b6628a2
silex189 <antoniojm89@gmail.com>
2018-10-31 18:49:00 +0000

    Autobox finished

---
+++ Book_utilities/src/chapter12_Enum_Autoboxing_staticImport_annotations/Quadratic.java
@@ -0,0 +1,10 @@
+package chapter12_Enum_Autoboxing_staticImport_annotations;
+
+public class Quadratic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
